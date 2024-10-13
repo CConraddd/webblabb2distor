cab3374fec9bcabf59a429769c299a7f0b11e846
kalste00 <kalle.stenbjelke@gmail.com>
2024-10-13 16:11:01 +0200

    Controller

---
+++ webblabb2distor/Controllers/AuctionControllers.cs
@@ -0,0 +1,82 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace webblabb2distor.Controllers
+{
+    public class AuctionControllers : Controller
+    {
+        // GET: AuctionControllers
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        // GET: AuctionControllers/Details/5
+        public ActionResult Details(int id)
+        {
+            return View();
+        }
+
+        // GET: AuctionControllers/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: AuctionControllers/Create
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: AuctionControllers/Edit/5
+        public ActionResult Edit(int id)
+        {
+            return View();
+        }
+
+        // POST: AuctionControllers/Edit/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit(int id, IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: AuctionControllers/Delete/5
+        public ActionResult Delete(int id)
+        {
+            return View();
+        }
+
+        // POST: AuctionControllers/Delete/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Delete(int id, IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
